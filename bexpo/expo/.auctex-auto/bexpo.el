4443a1da8d12db6412a0a1db5877b66531cbcb16
Martin Sevov <ms.sevov@gmail.com>
2025-06-29 18:01:19 +0200

    Finished Bachelor Expose

--- bexpo/expo/.auctex-auto/bexpo.el
+++ bexpo/expo/.auctex-auto/bexpo.el
@@ -6,9 +6,13 @@
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("imc-inf" "")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("listings" "") ("subcaption" "") ("graphicx" "") ("url" "")))
+                     '(("listings" "") ("subcaption" "") ("graphicx" "") ("url" "") ("longtable" "") ("array" "")))
    (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
    (TeX-run-style-hooks
     "latex2e"
@@ -17,7 +21,11 @@
     "listings"
     "subcaption"
     "graphicx"
-    "url")
+    "url"
+    "longtable"
+    "array")
+   (TeX-add-symbols
+    "cleardoublepage")
    (LaTeX-add-bibliographies
     "references"))
  :latex)
---
+++ bexpo/expo/.auctex-auto/references.el
@@ -0,0 +1,9 @@
+;; -*- lexical-binding: t; -*-
+
+(TeX-add-style-hook
+ "references"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "batterham2020mental"))
+ '(or :bibtex :latex))
+
